f91fbb47f04430206c2f08dc7afa632698e5a0f1
Rahulkishore1995 <96075939+Rahulkishore1995@users.noreply.github.com>
2022-03-09 02:20:41 +0530

    Add files via upload
    tq

---
+++ division.js
@@ -0,0 +1,4 @@
+function division(a,b){
+    console.log(a/b);
+  }
+  module.exports=division;
---
+++ return.js
@@ -0,0 +1,17 @@
+const addition = require("./addition");
+console.log(addition(10,20));
+
+let x=addition(10,20);
+console.log(x);
+
+
+const multiply = require("./multiply");
+console.log(multiply(2,6));
+
+const division=require("./division");
+console.log(division(16,2));
+
+
+const subtract=require("./subtract");
+console.log(subtract(12,30));
+
